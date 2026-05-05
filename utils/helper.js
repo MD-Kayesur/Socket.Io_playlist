1b0c296254f24bf6f1bc817d02278e19bfa33f80
MD-Kayesur <rmdkayesur@gmail.com>
2026-05-05 23:05:50 +0600

    add order validation and ID generation helper utilities

--- utils/helper.js
+++ utils/helper.js
@@ -18,8 +18,21 @@ export function validateOrder(data){
         return {isValid:false, message: "Items is required"}
      }
      for(const item of data.items){
-        
+
      }
 
     return {isValid:true, message:"Valid Order"}
+}
+//order id generator -> formate : ORD-1749216465721
+export function generateOrderId(){
+    const now = new Date()
+    const year = now.getFullYear()
+    const month = String(now.getMonth() + 1).padStart(2,"0")
+    const day = String(now.getDate()).padStart(2,"0")
+    const hour = String(now.getHours()).padStart(2,"0")
+    const min = String(now.getMinutes()).padStart(2,"0")
+    const second = String(now.getSeconds()).padStart(2,"0")
+    const milliseconds = String(now.getMilliseconds()).padStart(3,"0")
+    const random = Math.floor(1000+Math.random() * 9000)
+    const dayHourMinuteSecond = 
 }
